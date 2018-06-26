b559954bf5b9febbc25abb785c4abc52b963db2d
Edson Onildo <inbox.edsononildo@gmail.com>
2018-06-26 20:38:02 -0300

    Build folder added to gitignore file

---
+++ src/CPack/Array.c
@@ -0,0 +1,5 @@
+//
+// Created by edsononildo on 25/06/18.
+//
+
+#include "Array.h"
---
+++ src/CPack/Array.h
@@ -0,0 +1,8 @@
+//
+// Created by edsononildo on 25/06/18.
+//
+
+#ifndef CPACK_ARRAY_H
+#define CPACK_ARRAY_H
+
+#endif //CPACK_ARRAY_H
